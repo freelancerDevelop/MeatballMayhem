167f64e73d8100c25c24b933dfc2c6d9778a0ce1
Thomas Loupe <acidxrain@users.noreply.github.com>
2018-10-18 22:59:06 -0500

    10/18/2018
    Minor changes to Twitch client script. No more update calls. Removed some debug code/comments.

--- Assets/Scripts/TwitchClient.cs
+++ Assets/Scripts/TwitchClient.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class TwitchClient : MonoBehaviour
 {
-    //public static TwitchClient Instance;
     private Client _client;
     public PlayerManager pm;
     public RoundManager rm;
@@ -18,7 +17,7 @@ public class TwitchClient : MonoBehaviour
 
     private void Start()
     {
-        // Make the application run in the background so we don't disconnect from Twitch.
+        // Make the application run in the background so we don't disconnect from Twitch
         Application.runInBackground = true;
 
         string _channelToConnectTo = PlayerPrefs.GetString("CHANNEL_NAME"); //SetSecrets.Instance.USERNAME_FROM_OAUTH_TOKEN;
@@ -29,23 +28,19 @@ public class TwitchClient : MonoBehaviour
         // Create new instance of Chat Client
         _client = new Client();
 
-        // Initialize the client with the credentials instance, and setting a default channel to connect to.
+        // Initialize the client with the credentials instance, and setting a default channel to connect to
         _client.Initialize(credentials, _channelToConnectTo);
 
         // Bind callbacks to events
         _client.OnConnected += OnConnected;
         _client.OnJoinedChannel += OnJoinedChannel;
         _client.OnMessageReceived += OnMessageReceived;
-        //_client.OnChatCommandReceived += OnChatCommandReceived;
         _client.OnWhisperCommandReceived += OnWhisperCommandReceived;
 
         // Connect
         _client.Connect();
-    }
 
-    private void Update()
-    {
-        //Check to see if the game is joinable.
+        // Check to see if the game is joinable
         StartCoroutine(pm.CheckForMaxPlayers(pm.atMaxPlayers));
     }
 
@@ -75,38 +70,40 @@ public class TwitchClient : MonoBehaviour
         switch (e.Command.CommandText)
         {
             case "hello":
-                _client.SendMessage(e.Command.WhisperMessage.DisplayName, $"Whale hello there, {e.Command.WhisperMessage.DisplayName}!");
+                _client.SendWhisper(e.Command.WhisperMessage.DisplayName, $"Whale hello there, {e.Command.WhisperMessage.DisplayName}!");
                 break;
 
             case "about":
-                _client.SendMessage(e.Command.WhisperMessage.DisplayName, $"Running Meatball Mayhem v0.9a! Whisper !commands to view the game commands or download the app!");
+                _client.SendWhisper(e.Command.WhisperMessage.DisplayName, $"Running Meatball Mayhem v0.9a! Whisper !commands to view the game commands or download the app!");
                 break;
 
             default:
-                _client.SendMessage(e.Command.WhisperMessage.DisplayName, $"Unknown chat command: {e.Command.CommandIdentifier}{e.Command.CommandText}");
+                _client.SendWhisper(e.Command.WhisperMessage.DisplayName, $"Unknown chat command: {e.Command.CommandIdentifier}{e.Command.CommandText}");
                 break;
 
             case "commands":
-                _client.SendMessage(e.Command.WhisperMessage.DisplayName, $"Commands: !hello, !about, !join, !moveleft, !moveright, !jumpleft, !jumpright, !aim, !fire");
+                _client.SendWhisper(e.Command.WhisperMessage.DisplayName, $"Commands: !hello, !about, !join, !moveleft, !moveright, !jumpleft, !jumpright, !aim, !fire");
                 break;
 
             case "join":
 
                 Debug.Log("Checking for max players...");
+                // Check to see if the game is joinable
+                StartCoroutine(pm.CheckForMaxPlayers(pm.atMaxPlayers));
 
                 if (debugMode == false && pm.playerList.Contains(name))
                 {
-                    _client.SendMessage(e.Command.WhisperMessage.DisplayName, $"You've already joined and can't join again!");
+                    _client.SendWhisper(e.Command.WhisperMessage.DisplayName, $"You've already joined and can't join again!");
                     break;
                 }
 
                 if (pm.atMaxPlayers)
                 {
-                    _client.SendMessage(e.Command.WhisperMessage.DisplayName, $"Game is now full! Try joining after the game is over!");
+                    _client.SendWhisper(e.Command.WhisperMessage.DisplayName, $"Game is now full! Try joining after the game is over!");
                     Debug.Log("A chatter tried to join the game but game is full...");
                     break;
                 }
-                _client.SendMessage(e.Command.WhisperMessage.DisplayName, $"Joining the game!");
+                _client.SendWhisper(e.Command.WhisperMessage.DisplayName, $"Joining the game!");
                 AddNewPlayer(name);
                 AddToFired(gameObject);
 
@@ -115,6 +112,10 @@ public class TwitchClient : MonoBehaviour
                 AddToAlive(name);
 
                 Debug.Log("Spawning " + name + "!");
+
+                // Check to see if the game is joinable
+                // We do this again after a player joins to see if they're taking the last open player spot
+                StartCoroutine(pm.CheckForMaxPlayers(pm.atMaxPlayers));
 
                 break;
             
@@ -155,18 +156,18 @@ public class TwitchClient : MonoBehaviour
                         }
                         else
                         {
-                            // The value specified was either too low or too high. Ignore the command.
+                            // The value specified was either too low or too high. Ignore the command
                         }
                     }
                     else
                     {
-                        // Do nothing, because we're in the attack round.
+                        // Do nothing, because we're in the attack round
                         _client.SendMessage(e.Command.WhisperMessage.DisplayName, $"You can't do this in the attack round.");
                     }
                 }
                 else
                 {
-                    // The person typing the command is either dead or isn't a current player.
+                    // The person typing the command is either dead or isn't a current player
                 }
                 break;
 
@@ -177,7 +178,7 @@ public class TwitchClient : MonoBehaviour
                 {
                     // Check if the player is alive first
                     bool isPlayerAlive = GameObject.Find(name).GetComponentInChildren<Health>().alive;
-                    //First we check if we're in the move round.
+                    //First we check if we're in the move round
                     if (rm.phase == "Move" && isPlayerAlive)
                     {
                         Rigidbody2D storedRB = GameObject.Find(name).GetComponentInChildren<Rigidbody2D>();
@@ -207,7 +208,7 @@ public class TwitchClient : MonoBehaviour
                         }
                         else
                         {
-                            // The value specified was either too low or too high. Ignore the command.
+                            // The value specified was either too low or too high. Ignore the command
                             _client.SendWhisper(e.Command.WhisperMessage.DisplayName, $"Did you mistype a command?");
                         }
                     }
@@ -229,7 +230,7 @@ public class TwitchClient : MonoBehaviour
                 {
                     bool isPlayerAlive = GameObject.Find(name).GetComponent<Health>().alive;
 
-                    // First we check if we're in the attack round.
+                    // First we check if we're in the attack round
                     if (rm.phase == "Attack" && isPlayerAlive)
                     {
                         GameObject playerGO = GameObject.Find(name);
@@ -238,23 +239,23 @@ public class TwitchClient : MonoBehaviour
                         int aimAngleInt;
                         int.TryParse(aimAngle, out aimAngleInt);
 
-                        // Do a check to see if the angle is within physics boundaries. If not, break.
+                        // Do a check to see if the angle is within physics boundaries.
                         if (aimAngleInt >= 0 && aimAngleInt <= 360)
                         {
-                            // If the angle is acceptable, set the aim angle for the player.
-                            // Get the player's instantiated prefab, and set angle on the attached aim script.
+                            // If the angle is acceptable, set the aim angle for the player
+                            // Get the player's instantiated prefab, and set angle on the attached aim script
                             _client.SendWhisper(e.Command.WhisperMessage.DisplayName, $"Aiming...");
                             playerGO.GetComponent<Aim>().SetAimToAngle(aimAngleInt);
                         }
                         else
                         {
-                            // The value specified was either too low or too high. Ignore the command.
+                            // The value specified was either too low or too high. Ignore the command
                             _client.SendWhisper(e.Command.WhisperMessage.DisplayName, $"Did you mistype a command?");
                         }
                     }
                     else
                     {
-                        // Do nothing, because we're in the move round.
+                        // Do nothing, because we're in the move round
                         _client.SendWhisper(e.Command.WhisperMessage.DisplayName, $"You can't aim in the move round.");
                     }
                 }
@@ -272,17 +273,13 @@ public class TwitchClient : MonoBehaviour
                         int forceValueInt;
                         int.TryParse(forceValue, out forceValueInt);
 
-                        // Check the player's gameobject script called Fired to see if they fired already.
-
-                        // THIS IS CAUSING A BUG FOR PLAYERS! THEY CAN'T FIRE IF SOMEONE ELSE HAS FIRED ALREADY!!!
-                        // var playerFiredScript = GameObject.Find(name).GetComponent<Fired>();
-                        // Try it this way instead...
+                        // Check the player's gameobject script called Fired to see if they fired already
                         bool playerFiredBool = GameObject.Find(name).GetComponent<Fired>().fired;
 
                         // Do a check to see if the player already fired
                         if (playerFiredBool == false)
                         {
-                            // Do a check to see if the value is within reasonable bounds. If not, break.
+                            // Do a check to see if the value is within reasonable bounds.
                             if (forceValueInt > 0 && forceValueInt <= 5)
                             {
                                 _client.SendWhisper(e.Command.WhisperMessage.DisplayName, $"Firing!");
@@ -291,7 +288,7 @@ public class TwitchClient : MonoBehaviour
                                 // Fire the explosive at the forceValue specified
                                 bomb.ThrowBomb(forceValueInt);
 
-                                // Set the fired bool to true so they can't fire again until the next round".
+                                // Set the fired bool to true so they can't fire again until the next round"
                                 playerFiredBool = true;
                             }
                             else
